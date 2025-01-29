2c39c3e5ad83c08bcd86e72d394d9aa32167e0ed
vyacheslav.belchuk <vyacheslav.belchuk@gmail.com>
2025-01-29 19:26:45 +0300

    Added new version of homepage: written on vue.js

--- src/Controller/HymnsController.php
+++ src/Controller/HymnsController.php
@@ -2,55 +2,27 @@
 
 namespace App\Controller;
 
-use App\Service\BookService;
-use App\Service\HymnService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HymnsController extends Controller
 {
-    public const DEFAULT_BOOK = 'song-of-rebirth-ehvda';
-
-    public const RESULTS_PER_PAGE = 100;
-
-    private HymnService $hymnService;
-
-    private BookService $bookService;
-
-    public function __construct(HymnService $hymnService, BookService $bookService)
+    #[Route('/', name: 'homepage', methods: ['GET', 'HEAD'])]
+    public function homepage(): Response
     {
-        $this->hymnService = $hymnService;
-        $this->bookService = $bookService;
+        return $this->render('hymns/main.html.twig');
     }
 
-    #[Route("/{bookId}/{page<\d+>}", name: "hymns", methods: ["GET", "HEAD"])]
-    public function index(string $bookId = self::DEFAULT_BOOK, int $page = 1): Response
+    #[Route('/{bookId}/{page<\d+>}', name: 'hymns', methods: ['GET', 'HEAD'])]
+    public function redirectToHomepage(string $bookId, int $page = 1): Response
     {
-        $booksResultDto = $this->bookService->getBooks();
-        $hymnsResultDto = $this->hymnService->getHymnsByBookId(
-            $bookId,
-            $page * self::RESULTS_PER_PAGE - self::RESULTS_PER_PAGE,
-            $page * self::RESULTS_PER_PAGE,
-        );
-        $books = $booksResultDto->getData();
-        $totalSongs = $books[$bookId]['total_songs'] ?? 100;
-
-        return $this->render('hymns/hymns.html.twig', [
-            'bookId'         => $bookId,
-            'page'           => $page,
-            'resultsPerPage' => self::RESULTS_PER_PAGE,
-            'totalSongs'     => $totalSongs,
-            'books'          => $booksResultDto->getData(),
-            'hymns'          => $hymnsResultDto->getData(),
-        ]);
+        return $this->redirect($this->generateUrl('homepage'));
     }
 
-    #[Route("/hymns/book/{bookId}/{page<\d+>}", name: "hymns.redirect", methods: ["GET", "HEAD"])]
-    public function redirectToIndex(string $bookId = self::DEFAULT_BOOK, int $page = 1): RedirectResponse
+    #[Route('/hymns/book/{bookId}/{page<\d+>}', name: 'hymns.redirect', methods: ['GET', 'HEAD'])]
+    public function redirectToIndex(string $bookId = '', int $page = 1): RedirectResponse
     {
-        return $this->redirect(
-            $this->generateUrl('hymns', ['bookId' => $bookId, 'page' => $page])
-        );
+        return $this->redirect($this->generateUrl('homepage'));
     }
 }
