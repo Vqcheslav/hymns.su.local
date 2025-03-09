6a8856e1a0c523181a57c9bf0b874e9e939abe13
vyacheslav.belchuk <vyacheslav.belchuk@gmail.com>
2025-03-10 00:37:55 +0300

    [Feature] Added telegram bot

--- src/Service/HymnsTelegramBotService.php
+++ src/Service/HymnsTelegramBotService.php
@@ -6,29 +6,95 @@ use App\Dto\ResultDto;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Throwable;
 
-use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
-
 class HymnsTelegramBotService extends Service
 {
     public function __construct(
         private readonly HttpClientInterface $httpClient,
+        private readonly HymnService $hymnService,
     ) {}
 
     public function processMessage(array $data): ResultDto
     {
-        $token = $_SERVER['TELEGRAM_BOT_TOKEN'];
-        $queryParams = [
-            'chat_id' => $data['message']['chat']['id'],
-            'text'    => $data['message']['text'],
-        ];
+        $messageText = $data['message']['text'];
 
+        if (str_starts_with($messageText, '/')) {
+            $hymnId = str_replace('_', '-', substr($messageText, 1));
+            $hymnResultDto = $this->hymnService->getHymnByHymnId($hymnId);
+
+            if ($hymnResultDto->hasErrors()) {
+                $resultMessage = $hymnResultDto->getDetail();
+            } else {
+                $resultMessage = $this->getTextFromHymn($hymnResultDto->getData());
+            }
+        } else {
+            $hymnsResultDto = $this->hymnService->searchHymns($messageText);
+            $hymns = $hymnsResultDto->getData();
+
+            if ($hymnsResultDto->hasErrors()) {
+                $resultMessage = $hymnsResultDto->getDetail();
+            } elseif (empty($hymns)) {
+                $resultMessage = 'Ничего не найдено';
+            } else {
+                $resultMessage = $this->getMessageWithActionsFromHymnsArray($hymns);
+            }
+        }
+
+        return $this->sendMessage($data['message']['chat']['id'], $resultMessage, $data['message']['message_id']);
+    }
+
+    public function getMessageWithActionsFromHymnsArray(array $hymns): string
+    {
+        $result = [];
+
+        foreach ($hymns as $hymn) {
+            $action = '/' . str_replace('-', '_', $hymn['hymn_id']);
+            $result[] = sprintf("%s\n%s", $this->getHeaderOfHymn($hymn), $action);
+        }
+
+        return implode("\n\n", $result);
+    }
+
+    public function getTextFromHymn(array $hymn): string
+    {
+        $lyrics = '';
+
+        foreach ($hymn['verses'] as $verse) {
+            if ($verse['is_chorus']) {
+                $lyrics .= sprintf("Припев:\n%s", $verse['lyrics']);
+            } else {
+                $lyrics .= sprintf('%d. %s', $verse['position'], $verse['lyrics']);
+            }
+
+            $lyrics .= "\n\n";
+        }
+
+        return sprintf("%s\n\n\n%s", $this->getHeaderOfHymn($hymn), $lyrics);
+    }
+
+    public function getHeaderOfHymn(array $hymn): string
+    {
+        $bookTitleAndCategory = sprintf('%s :: %s', $hymn['book_title'], $hymn['category']);
+        $numberAndTitle = sprintf('%d: %s…', $hymn['number'], $hymn['title']);
+
+        return sprintf("%s\n%s", $bookTitleAndCategory, $numberAndTitle);
+    }
+
+    private function sendMessage(int $chatId, string $text, int $replyTo = null, string $parseMode = 'html'): ResultDto
+    {
         try {
+            $responseQueryParams = [
+                'chat_id'             => $chatId,
+                'text'                => $text,
+                'parse_mode'          => $parseMode,
+                'reply_to_message_id' => $replyTo,
+            ];
+
             $this->httpClient->request(
                 'GET',
                 sprintf(
                     'https://api.telegram.org/bot%s/sendMessage?%s',
-                    $token,
-                    http_build_query($queryParams),
+                    $_SERVER['TELEGRAM_BOT_TOKEN'],
+                    http_build_query($responseQueryParams),
                 ),
             );
         } catch (Throwable $e) {
