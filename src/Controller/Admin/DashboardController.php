f7e8665fdf28538a4718bb10270d9da445a4c0da
vyacheslav.belchuk <vyacheslav.belchuk@gmail.com>
2025-04-10 15:40:57 +0300

    [Fix] Fixed redirect to the default admin page

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,7 +9,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
@@ -17,9 +16,7 @@ class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-
-        return $this->redirect($adminUrlGenerator->setController(HymnCrudController::class)->generateUrl());
+        return $this->redirectToRoute('admin_book_index');
     }
 
     public function configureDashboard(): Dashboard
