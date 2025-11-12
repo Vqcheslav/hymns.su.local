ddffce3e69767f198217e64893f48474f995c38a
vyacheslav.belchuk <vyacheslav.belchuk@gmail.com>
2025-11-12 17:54:15 +0300

    [Fix] Removed the 'Delete book' button

--- src/Controller/Admin/BookCrudController.php
+++ src/Controller/Admin/BookCrudController.php
@@ -29,7 +29,8 @@ class BookCrudController extends AbstractCrudController
         return $actions
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
             ->remove(Crud::PAGE_INDEX, Action::NEW)
-            ->remove(Crud::PAGE_INDEX, Action::DELETE);
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+            ->remove(Crud::PAGE_DETAIL, Action::DELETE);
     }
 
     public function configureFields(string $pageName): iterable
