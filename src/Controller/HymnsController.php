275136d19644b38150987c1e4aaeb81ce4e740fb
vyacheslav.belchuk <vyacheslav.belchuk@gmail.com>
2026-08-20 20:09:12 +0300

    [Feature] Added redirect to main link

--- src/Controller/HymnsController.php
+++ src/Controller/HymnsController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HymnsController extends Controller
 {
     #[Route('/', name: 'homepage', methods: ['GET', 'HEAD'])]
-    public function homepage(): Response
+    public function homepage(Request $request): Response
     {
+        $url = $request->getUri();
+
+        if (str_contains($url, 'hymns.su')) {
+            return $this->redirect(str_replace('hymns.su', 'psalma.name', $url), Response::HTTP_MOVED_PERMANENTLY);
+        }
+
         return $this->render('hymns/main.html.twig');
     }
 }
